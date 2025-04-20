6342f66852a7e3cbdd3d6af48abe525ca6f3410a
smith13713 <cailinchristie13@gmail.com>
2025-04-20 09:53:20 +0200

    Initial commit: add existing Java project

---
+++ Assignment4/src/main/java/assignment4/Connect.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package assignment4;
+
+/**
+ *
+ * @author caili
+ */
+public class Connect {
+
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//    }
+}
